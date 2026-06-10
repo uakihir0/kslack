b344826d0ba7bbb01a11bb6ea9615c261f529f09
Akihiro Urushihara <a.urusihara@gmail.com>
2026-06-11 01:57:33 +0900

    Revert "Revert "Remove deprecated macosX64 target""

--- all/build.gradle.kts
+++ all/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     jvmToolchain(11)
     jvm()
 
-    js(IR) {
+    js {
         nodejs()
         browser()
         binaries.library()
@@ -27,7 +27,6 @@ kotlin {
         iosX64(),
         iosArm64(),
         iosSimulatorArm64(),
-        macosX64(),
         macosArm64(),
     ).forEach {
         it.binaries.framework {
